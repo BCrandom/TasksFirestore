d1e0ae02c674ad25e7d814d0d0f8b893fc2c74e9
BCrandom <coreabarbara306@gmail.com>
2024-06-09 18:08:01 -0400

    “tasksFirestore”

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -26,7 +26,7 @@ import {
     doc,
     getDoc,
     updateDoc
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
 
 // Conexcion con la base de datos
 const db = getFirestore();
